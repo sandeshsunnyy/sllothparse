bf339615c62470e062b8501fb566b68eadbe53ea
sandesh <sandeshsunny2020@gmail.com>
2025-10-16 18:31:06 +0530

    Arranged the style tuples into distinct groups. We may have to process each group induvidually to get best results, but thats alright. This is supposed to be a long project. The groups are defined by comparing size of common tuple to that of the others. Next step would be to assign specific tags to these tuples.

--- chunk_builder.py
+++ chunk_builder.py
@@ -1,6 +1,7 @@
 from pdfparser import PDFParser
 import fitz
 import traceback
+import collections
 
 """
 1. Take out text
@@ -12,6 +13,7 @@ import traceback
 
 pdf_path = "12 SEPTEMBER 2025.pdf"
 
+
 def main():
 
     try:
@@ -20,19 +22,37 @@ def main():
 
         previous_dict = {} 
         previous_chunk_no = None
-
+        all_styles = []
         for page in pages:
             blocks = page.get_text('dict')
             sorted_blocks = sorted(blocks["blocks"], key=lambda b: (b["bbox"][1], b["bbox"][0]))
             parser = PDFParser(sorted_blocks)
-            style_tuples = parser.getStyleTuples(blocks=sorted_blocks)
-            print(style_tuples)
-            '''tagged_spans = parser.tagPages(common_font_size=common_font_size, blocks=sorted_blocks)
-            current_dict, current_chunk_no = parser.createDict(previous_dict=previous_dict, previous_chunk_no=previous_chunk_no, tagged_spans=tagged_spans)
-            previous_dict, previous_chunk_no = current_dict, current_chunk_no'''
+            all_styles += parser.getStyleTuples(blocks=sorted_blocks)
+            
+        most_common = collections.Counter(all_styles).most_common(1)[0][0]
+        print(most_common) # do something for heading and sub-headings (quick-thought: count-based?)
+        print(f"most common= {most_common}")
+        distinct_styles = list(set(all_styles))
+        print(distinct_styles) # we got distinct styles
 
-        'print(previous_dict)'
-                  
+        #check for same font over all fonts if it is same fonts then a different logic has to be font. 
+        sorted_styles_on_size = sorted(distinct_styles, key= lambda item: (item[0], item[1]), reverse=True)
+        #Even after sorting if it is the same size then we should look for other ways to find semantics.
+        print(sorted_styles_on_size)
+
+        #divide the tuples into different groups based on whether it is in the same level as common text. In the group having the common tuple, place it at the very end. Anything below the common font should be handled carefully.
+        ix = sorted_styles_on_size.index(most_common)
+        larger, same, smaller = [], [], []
+        for index, style_tuple in enumerate(sorted_styles_on_size):
+            if index == ix:
+                continue
+            if style_tuple[0] == most_common[0]:
+                same.append(style_tuple)
+            elif style_tuple[0] > most_common[0]:
+                larger.append(style_tuple)
+            else:
+                smaller.append(style_tuple)
+        same.append(most_common)
 
 
     except FileNotFoundError:
